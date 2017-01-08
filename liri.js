1f11823a561d94f6f07bf6a6852954cf712dd15e
rxtATX <rachel.thiim@gmail.com>
2017-01-07 18:04:18 -0600

    Close but no cigar on the do-this function, thinking about rearranging everything within for better functionality. Currently have certain substring elements working but removing plusses and quotations not working

--- liri.js
+++ liri.js
@@ -29,16 +29,16 @@ switch (action) {
 //Function which uses OMDB to pull information about title passed in by user.
 function findMovie() {
 	//Takes the value of the array in the [3] position.
-	var movieName = process.argv[3];
+	var queryInput = process.argv[3];
 	//Loop which will filter through user input and replace any spaces with + to pass into query URL.
-	for (var i = 0; i < movieName.length; i++) {
-		if (movieName.charAt(i) === " ") {
-			movieName = movieName.substring(0, i) + "+" + movieName.substring(i + 1);
+	for (var i = 0; i < queryInput.length; i++) {
+		if (queryInput.charAt(i) === " ") {
+			queryInput = queryInput.substring(0, i) + "+" + queryInput.substring(i + 1);
 		}
 	}
 
 	// Then run a request to the OMDB API with the movie specified.
-	var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&r=json";
+	var queryUrl = "http://www.omdbapi.com/?t=" + queryInput + "&y=&plot=short&r=json";
 
 	// Then create a request to the queryUrl.
 	request(queryUrl, function(error, response, body) {
@@ -67,29 +67,29 @@ function pullTweets() {
 //Function will process song input through Spotify API and return details.
 function songInfo() {
 	//Takes the value of the array in the [3] position.	
-	var songName = process.argv[3];
+	var queryInput = process.argv[3];
 	//Loop which will filter through user input and replace any spaces with + to pass into query URL.
-	for (var i = 0; i < songName.length; i++) {
-		if (songName.charAt(i) === " ") {
-			songName = songName.substring(0, i) + "+" + songName.substring(i + 1);
+	for (var i = 0; i < queryInput.length; i++) {
+		if (queryInput.charAt(i) === " ") {
+			queryInput = queryInput.substring(0, i) + "+" + queryInput.substring(i + 1);
 		}
 	}
 
 	// Then create a request to the queryUrl.
-    spotify.search({ type: 'track', query: songName }, 
+    spotify.search({ type: 'track', query: queryInput }, 
     function(err, data) {
+	    	//If there is an error:
+	    if (err) {
+            console.log('Error occurred: ' + err);
+            return;
+	    } else {
 		// If the request is successful.
-		if (!error && response.statusCode === 200) {
 		 	var results = data.tracks.items[0];
 		 	// Filter through the JSON object and recover pertinent information.
 		    console.log("Artist: " + results.artists[0].name);
 		    console.log("Song name: " + results.name);
 		    console.log("Listen here: " + results.preview_url);
 		    console.log("Found on album: " + results.album.name);
-	    } else {
-	    	//If there is an error:
-            console.log('Error occurred: ' + err);
-            return;
 	    }
     });
 };
@@ -99,13 +99,16 @@ function runRandomTxt() {
 	//File system to read random.txt file.
 	fs.readFile("random.txt", "utf8", function(err, data) {
 		//Separating the action portion from the search term portion of input in the txt file.
-		var newAction = data;
-		if (newAction.charAt(i) === ",") {
-			newAction = newAction.substring(0, i);
-			var newSearch = newAction.substring(i + 1);
+		for (var i = 0; i < data.length; i++) {
+			if (data.charAt(i) === ",") {
+				action = data.substring(0, i);
+			}
 		}
-		console.log(data);
-		console.log(newAction);
-		console.log(newSearch);
+		queryInput = data.substring(i + 1);
+		for (var i = 0; queryInput.length; i++) {
+			if (queryInput.charAt(i) === " ") {
+				queryInput = queryInput.substring(0, i) + "+" + queryInput.substring(i + 1);
+			}
+		}
 	})
 }
