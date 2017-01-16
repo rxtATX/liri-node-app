7434d9e44bca9d32f43f9d9e5516f136177aa053
Rachel <rachel.thiim@gmail.com>
2017-01-16 16:40:40 -0600

    Deleting keys.js from repo.

--- keys.js
+++
@@ -1,6 +0,0 @@
-exports.twitterKeys = {
-  consumer_key: 'eKZvocgWfY9eXtea7ELfOGp0c',
-  consumer_secret: 'cx4mLzrfipWl0zMqEMZw9rWPlvwwKKDWXZX71wQd3avs6724Pp',
-  access_token_key: '817836419116830724-7XUF7viBojlxWpTV127VxQfr7D54yK5',
-  access_token_secret: 'oSOieESLlMnyeHAxLjS9aliGZOZwBJzMrWEssZcq2A1CM',
-}
