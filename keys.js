d7591abecaf82d13c5b9557fdc6da8ec907ea6b6
rxtATX <rachel.thiim@gmail.com>
2017-01-16 16:48:14 -0600

    Trying to ensure keys.js doesn't appear on repo. Renewed my key gens so anything showing on repo is invalidated.

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 exports.twitterKeys = {
-  consumer_key: 'eKZvocgWfY9eXtea7ELfOGp0c',
-  consumer_secret: 'cx4mLzrfipWl0zMqEMZw9rWPlvwwKKDWXZX71wQd3avs6724Pp',
-  access_token_key: '817836419116830724-7XUF7viBojlxWpTV127VxQfr7D54yK5',
+  consumer_key: 'NGF9lDS4Lg3p4l3oariwcUQSs',
+  consumer_secret: 'Gjsh8VfRdBvMRDcwqHs2U9t3ZHHL3TYh9VY9bl4AgMRaDiWvFR',
+  access_token_key: '	817836419116830724-7XUF7viBojlxWpTV127VxQfr7D54yK5',
   access_token_secret: 'oSOieESLlMnyeHAxLjS9aliGZOZwBJzMrWEssZcq2A1CM',
 }
